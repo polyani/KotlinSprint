63451d71755b5a8774bb8ecfeed1d9186278962b
polyani <ybrgjk826@gmail.com>
2024-07-17 16:57:28 +0300

    KS-1-1 decision lesson1_task2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+    val sumOrders: Int = 75
+    val expGratitude: String = "Спасибо, за Ваш выбор! Надеемся на дальнейшее сотрудничество."
+    val amtWorkers: UInt = 1999U
+//    var amtWorkers: UInt = 2000U // обьявить изм.перем.
+
+    println(sumOrders)
+    println(expGratitude)
+
+//    --amtWorkers //декремент
+    println("Ваши $amtWorkers сотрудников.")
+
+}
