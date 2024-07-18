83725d9a44602395ce0d988ac5f3f2b56ead317d
polyani <ybrgjk826@gmail.com>
2024-07-18 13:05:05 +0300

    KS-1-3 вычищены не нужные комментарии

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,19 +1,11 @@
 package lesson_1
 
 fun main() {
-//    val sumOrders: Int = 75
-//    val expGratitude: String = "Спасибо, за Ваш выбор! Надеемся на дальнейшее сотрудничество."
-//    val amtWorkers: UInt = 1999U
-//    var amtWorkers: UInt = 2000U // обьявить изм.перем.
+
     val fstFlight: String = "1961"
     var flHour: String = "09"
     var flMinute: String = "07"
 
-//    println(sumOrders)
-//    println(expGratitude)
-
-//    --amtWorkers //декремент
-//    println("Ваши $amtWorkers сотрудников.")
     println("""
         Год полёта - $fstFlight
         Час старта - $flHour
