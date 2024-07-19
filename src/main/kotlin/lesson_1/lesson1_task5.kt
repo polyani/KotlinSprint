cf63122eb4ececc38d268d6ec70662bceb875e7f
polyani <ybrgjk826@gmail.com>
2024-07-19 14:22:38 +0300

    KS-1-5 код переписан.

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,15 @@
 package lesson_1
 
 fun main(){
-    val oneHour = 3_600
-    val oneMinute = 60
-    val oneSecond = 1
+
     val totalSecond = 6480
-    val fligtHour = totalSecond / oneHour // вывести кол-во целых часов, дробная часть будет отброшена
-    val fligtMinutes = totalSecond / oneMinute % oneMinute
-    val fligtSeconds = totalSecond % oneMinute
+    val fligtHour = totalSecond / ONE_HOUR // вывести кол-во целых часов, дробная часть будет отброшена
+    val fligtMinutes = totalSecond / ONE_MINUTE % ONE_MINUTE
+    val fligtSeconds = totalSecond % ONE_MINUTE
 
    println(String.format("%02d:%02d:%02d", fligtHour, fligtMinutes, fligtSeconds))
 }
+
+const val ONE_HOUR = 3600
+const val ONE_MINUTE = 60
+const val ONE_SECOND = 1
