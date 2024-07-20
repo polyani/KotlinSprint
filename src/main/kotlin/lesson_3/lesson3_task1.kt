84473a42d243395d99441b015fa040e25e1bb852
polyani <ybrgjk826@gmail.com>
2024-07-20 15:46:08 +0300

    KS-3-1 решено удалено лишнее

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -8,21 +8,3 @@ fun main(){
     println("$welcomePrint $userName")
 
 }
-
-//import java.sql.Timestamp
-//
-//fun main(){
-//    print("Пожалуста введите имя - ")
-//    val nameUser = readln()
-//    val greetinMoning = "Доброе утро $nameUser"
-//    val greetinEvening = "Добрый вечер $nameUser"
-//    val currentTimeMillis = System.currentTimeMillis()
-//    val timeStamp = Timestamp(currentTimeMillis).toString()
-//    val curTime = timeStamp.substring(11,13).toInt()
-//    if(curTime < 12)
-//    println("$greetinMoning")
-//    else
-//    println("$greetinEvening")
-//
-//}
-
