f36badb8fb480ff0f9fb70340e7b76cdc33ee393
polyani <ybrgjk826@gmail.com>
2024-07-20 17:07:43 +0300

    KS-3-3 решено

---
+++ src/main/kotlin/lesson3_task3.kt
@@ -0,0 +1,18 @@
+package lesson_3
+
+fun main(){
+    print("Введите число - ")
+    val number = readln().toShort()
+
+        println(
+            "$number * 1 = " + (number * 1) + "\n"
+          + "$number * 2 = " + (number * 2) + "\n"
+          + "$number * 3 = " + (number * 3) + "\n"
+          + "$number * 4 = " + (number * 4) + "\n"
+          + "$number * 5 = " + (number * 5) + "\n"
+          + "$number * 6 = " + (number * 6) + "\n"
+          + "$number * 7 = " + (number * 7) + "\n"
+          + "$number * 8 = " + (number * 8) + "\n"
+          + "$number * 9 = " + (number * 9) + "\n"
+          + "$number * 10 = " + (number * 10) + "\n")
+}
