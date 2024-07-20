87f209f494f811bdc23b1a08ecff90a21563df55
polyani <ybrgjk826@gmail.com>
2024-07-20 12:07:20 +0300

    KS-2-5 решено

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,21 +1,16 @@
 package lesson_2
 
-// import kotlin.math.*
+import kotlin.math.*
+
 
 fun main(){
 
-    val deposit = 70000 // первоначальная сумма
-    var yearPercent: Float = 16.7f // процентная ставка
-    val depositPeriod = 20 // срок вклада в годах
-    var totalAmount: Float // итоговая сумма
-    var result: Float = (1 + (16.7/100)).pow(20)
+    val deposit = 70000
+    val yearPercent = 16.7
+    val depositPeriod = 20
+    val totalAmount: Double
+    totalAmount = (1 + (yearPercent/100)).pow(depositPeriod) * deposit
 
-    totalAmount = (deposit * result)
+    println("%.3f".format(totalAmount))
 
-    println(totalAmount)
-
-    // формула сумма = x * (1 + m/100) в степени n
-    // x - которую положили на вклад
-    // м - годовая процентная ставка
-    // n - срок действия вклада
 }
