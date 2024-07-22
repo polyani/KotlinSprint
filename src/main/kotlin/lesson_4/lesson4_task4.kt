2be4926dcbe78448103c68fe4668aabb17b04f23
polyani <ybrgjk826@gmail.com>
2024-07-22 14:55:57 +0300

    KS-4-4 reformat commit

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,18 +1,18 @@
 package lesson_4
 
-fun main(){
+fun main() {
 
-    var counter = 5
+    var counter = 2
     counter = ++counter % 2
-    var comparisonCounter: Boolean
-    comparisonCounter = counter == DEF_COUNTER_HAND
 
-    println("""
+    println(
+        """
         $TRAING_HAND ${counter == DEF_COUNTER_HAND} 
         $TRAING_LEG ${counter == DEF_COUNTER_LEG}
         $TRAING_BACK ${counter == DEF_COUNTER_BACK}
         $TRAING_ABC ${counter == DEF_COUNTER_ABC}
-    """.trimIndent())
+    """.trimIndent()
+    )
 
 }
 
@@ -21,6 +21,6 @@ const val DEF_COUNTER_LEG = 1
 const val DEF_COUNTER_BACK = 1
 const val DEF_COUNTER_ABC = 0
 const val TRAING_HAND = "Упражнения для рук:  "
-const val TRAING_LEG =  "Упражнения для ног:  "
+const val TRAING_LEG = "Упражнения для ног:  "
 const val TRAING_BACK = "Упражнения для спины:"
-const val TRAING_ABC =  "Упражнения для спины:"
+const val TRAING_ABC = "Упражнения для спины:"
