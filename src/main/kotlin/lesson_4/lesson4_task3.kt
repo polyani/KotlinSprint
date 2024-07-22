47ad3380b898cdc6aac729de7520e18a22e6b6c0
polyani <ybrgjk826@gmail.com>
2024-07-22 21:04:20 +0300

    KS-4-3 reformat commit

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,15 +6,17 @@ fun main() {
     val posTent: Boolean = true
     val humidAir = 20
     val season = ""
-    val compConditios: Boolean = (availTheSun == THE_SUN) && (posTent == THE_TENT_OPEN) &&
-            (humidAir == HUMID) && !(season == THE_SEASON_4)
+    val compConditios: Boolean = availTheSun == isSunny &&
+                                posTent ==  isAwningOpen &&
+                                humidAir == HUMID &&
+                                season != THE_SEASON_4
 
     println("$DEF_TEXT $compConditios")
 
 }
 
-const val THE_SUN = true
-const val THE_TENT_OPEN = true
+const val isSunny = true
+const val  isAwningOpen = true
 const val HUMID = 20
 const val THE_SEASON_4 = "winter"
 const val DEF_TEXT = "Благоприятные ли условия сейчас для роста бобовых? -"
