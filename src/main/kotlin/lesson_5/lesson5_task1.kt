c62d8f11ebba82a89ef6497a07a18d90ec081f3b
polyani <ybrgjk826@gmail.com>
2024-07-23 18:37:58 +0300

    KS-5-1 decided commit

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,12 @@
+package lesson_5
+
+fun main() {
+
+    print("Введите сумму чисел 5 и 6 = ")
+    val inSum: Int = readln().toInt()
+
+    if(inSum == 11){
+        println("Добро пожаловать!")
+    }
+    else print("Доступ запрещён")
+}
