6f40d1acec211ee6014832930f923b8a8970197f
polyani <ybrgjk826@gmail.com>
2024-07-24 12:26:18 +0300

    KS-5-3 corrected commit

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,18 +1,20 @@
 package lesson_5
 
-fun main(){
+fun main() {
     print("Введите первое целое число от 0 до 42 - ")
     val inNumber1 = readln().toInt()
     print("Введите второе целое число от 0 до 42 - ")
     val inNumber2 = readln().toInt()
-    val resultText = if (inNumber2 == IS_WIN_NUMBER_2 && inNumber1 == IS_WIN_NUMBER_1) "Поздравляем! Вы выиграли главный приз!"
-    else if (inNumber2 == IS_WIN_NUMBER_2 && inNumber1 != IS_WIN_NUMBER_1) "Вы выиграли утешительный приз!"
-    else if (inNumber2 != IS_WIN_NUMBER_2 && inNumber1 == IS_WIN_NUMBER_1) "Вы выиграли утешительный приз!"
+    val sumNumber = inNumber1 + inNumber2
+    val resultText = if (sumNumber == (WIN_NUMBER_1 + WIN_NUMBER_2)) "Поздравляем! Вы выиграли главный приз!"
+    else if ((inNumber2 == WIN_NUMBER_2 && inNumber1 != WIN_NUMBER_1) ||
+            (inNumber2 != WIN_NUMBER_2 && inNumber1 == WIN_NUMBER_1)) "Вы выиграли утешительный приз!"
     else "Неудача!"
     println(resultText)
-    println("Выигрышные числа: $IS_WIN_NUMBER_1, $IS_WIN_NUMBER_2.")
+    println("Выигрышные числа: $WIN_NUMBER_1, $WIN_NUMBER_2.")
 
 }
-const val IS_WIN_NUMBER_1 = 18
-const val IS_WIN_NUMBER_2 = 25
+
+const val WIN_NUMBER_1 = 18
+const val WIN_NUMBER_2 = 25
 
