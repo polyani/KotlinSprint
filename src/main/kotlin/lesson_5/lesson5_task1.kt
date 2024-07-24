52ca94687ab9362f79f0f71d7886d853e3170a66
polyani <ybrgjk826@gmail.com>
2024-07-24 10:00:15 +0300

    KS-5-1 corrected commit

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -4,8 +4,9 @@ fun main() {
 
     print("Введите сумму чисел 5 и 6 = ")
     val inSum: Int = readln().toInt()
+    val corrSum = 11
 
-    if(inSum == 11){
+    if(inSum == corrSum){
         println("Добро пожаловать!")
     }
     else print("Доступ запрещён")
