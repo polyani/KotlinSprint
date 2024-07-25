031005642362530827922cf0f06ac33288b9e4c0
polyani <ybrgjk826@gmail.com>
2024-07-25 15:24:53 +0300

    KS-5-5 corrected commit

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,26 +1,29 @@
 package lesson_5
 
 fun main() {
-    val WinNumbers: List<Int> = listOf((0..14).random(), (15..28).random(), (29..42).random())
-    print("Введите первое число в диапазоне от 0 до 42: ")
+    val winNumbers: List<Int> = listOf((0..14).random(), (15..28).random(), (29..42).random())
+    println(winNumbers)
+    val sizeWinNumbers = winNumbers.size
+    print("Введите число в диапазоне от 0 до 42: ")
     val inNumberList: MutableList<Int> = mutableListOf(readln().toInt())
-    print("Введите второе число в диапазоне от 0 до 42: ")
-    inNumberList.add(readln().toInt())
-    print("Введите третье число в диапазоне от 0 до 42: ")
-    inNumberList.add(readln().toInt())
-    val commonNumbers = inNumberList.intersect(WinNumbers)
+    for (i in 1 until sizeWinNumbers) {
+        print("Введите число в диапазоне от 0 до 42: ")
+        inNumberList.add(readln().toInt())
+    }
+    val commonNumbers = inNumberList.intersect(winNumbers)
     val sizeCommonNumbers = commonNumbers.size
-    val sizeWinNumbers = WinNumbers.size
     val resultText =
-        if (sizeCommonNumbers == sizeWinNumbers) "Поздравляем! Вы выиграли главный приз!"
-        else if (sizeCommonNumbers == (sizeWinNumbers - 1)) "Вы угадали два числа и получаете крупный приз!"
-        else if (sizeCommonNumbers == (sizeWinNumbers - 2)) "Вы угадали одно число и выиграли утешительный приз!"
-        else "Неудача! Вы не угадали ни одного числа."
+        when {
+            (sizeCommonNumbers == sizeWinNumbers) -> "Поздравляем! Вы выиграли главный приз!"
+            (sizeCommonNumbers == (sizeWinNumbers - 1)) -> "Вы угадали два числа и получаете крупный приз!"
+            (sizeCommonNumbers == (sizeWinNumbers - 2)) -> "Вы угадали одно число и выиграли утешительный приз!"
+            else -> "Неудача! Вы не угадали ни одного числа."
+        }
     println(
         """
         $resultText
         
-        Выигрышные числа: $WinNumbers.
+        Выигрышные числа: $winNumbers.
         """.trimIndent()
     )
 }
