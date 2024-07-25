429ff68fc098d8c65772812000da7ceb84768138
polyani <ybrgjk826@gmail.com>
2024-07-25 13:33:26 +0300

    KS-5-3 corrected commit

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -5,10 +5,11 @@ fun main() {
     val inNumber1 = readln().toInt()
     print("Введите второе целое число от 0 до 42 - ")
     val inNumber2 = readln().toInt()
-    val sumNumber = inNumber1 + inNumber2
-    val resultText = if (sumNumber == (WIN_NUMBER_1 + WIN_NUMBER_2)) "Поздравляем! Вы выиграли главный приз!"
-    else if ((inNumber2 == WIN_NUMBER_2 && inNumber1 != WIN_NUMBER_1) ||
-            (inNumber2 != WIN_NUMBER_2 && inNumber1 == WIN_NUMBER_1)) "Вы выиграли утешительный приз!"
+    val resultText = if ((inNumber1 == WIN_NUMBER_1 && inNumber2 == WIN_NUMBER_2) ||
+        (inNumber1 == WIN_NUMBER_2 && inNumber2 == WIN_NUMBER_1)) "Поздравляем! Вы выиграли главный приз!"
+    else if ((inNumber1 == WIN_NUMBER_1 && inNumber2 != WIN_NUMBER_2) ||
+        (inNumber1 == WIN_NUMBER_2 && inNumber1 != WIN_NUMBER_1) ||
+        (inNumber2 == WIN_NUMBER_1 && inNumber2 != WIN_NUMBER_2)) "Вы выиграли утешительный приз!"
     else "Неудача!"
     println(resultText)
     println("Выигрышные числа: $WIN_NUMBER_1, $WIN_NUMBER_2.")
