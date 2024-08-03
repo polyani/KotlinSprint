eb1468a7e6c3a5413faafa71a36909b423f4cd8b
polyani <ybrgjk826@gmail.com>
2024-08-03 18:48:21 +0300

    KS-7-2 Commit corrected

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,19 +1,39 @@
 package lesson_7
 
 fun main() {
-    var pinCode = (1000..9999).random()
+    var pinCode: Int = (1000..9999).random()
     println("Ваш код авторизации: $pinCode")
-    print("\nВведите ваш пин-код для авторизации: ")
+    print("Введите пин-код для авторизации: ")
     var inPinCode = readln().toInt()
-
-    while (true) {
-        if (inPinCode == pinCode) {
-            println("Введен правильный пин-код")
-            break
-        }
-        println("Пин-код ошибочен! Введите правильный: ")
-        pinCode = (1000..9999).random()
-        println("Ваш код авторизации: $pinCode")
-        inPinCode = readln().toInt()
+    if (pinCode == inPinCode) {
+        println("Введен правильный пин-код")
+    } else {
+        do {
+            println("Пин-код ошибочен!")
+            pinCode = (1000..9999).random()
+            println("Ваш новый код авторизации: $pinCode")
+            print("Введите пин-код для авторизации: ")
+            inPinCode = readln().toInt()
+        } while (inPinCode != pinCode)
+        println("Введен правильный пин-код")
     }
 }
+
+//
+//fun main() {
+//    var pinCode = (1000..9999).random()
+//    println("Ваш код авторизации: $pinCode")
+//    print("\nВведите ваш пин-код для авторизации: ")
+//    var inPinCode = readln().toInt()
+//
+//    while (true) {
+//        if (inPinCode == pinCode) {
+//            println("Введен правильный пин-код")
+//            break
+//        }
+//        println("Пин-код ошибочен! Введите правильный: ")
+//        pinCode = (1000..9999).random()
+//        println("Ваш код авторизации: $pinCode")
+//        inPinCode = readln().toInt()
+//    }
+//}
