02142abf69655035831069e88fc458b9b08ce5a4
polyani <ybrgjk826@gmail.com>
2024-08-03 19:26:16 +0300

    KS-7-1 Commit corrected

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,30 +1,13 @@
 package lesson_7
 
-
 fun main() {
 
-    val CharList = ('a'..'z').joinToString("")
+    val CharList = ('a'..'z')
+    val NumbersList = (0..9)
+    var resultPass: String = ""
 
-    val n = 9
-
-    val NumbersList = (0..n).joinToString("")//.toList()
-
-    val resulrPass: MutableList<String> = arrayListOf() //результирующий пароль
-
-    var passChr = CharList.random().toString()
-    var passNum = NumbersList.random().toString()
-
-    for (i in 1..6) {
-
-        if (i % 2 > 0) {
-            resulrPass.add(passChr)
-            passChr = CharList.random().toString()
-        } else {
-            resulrPass.add(passNum)
-            passNum = NumbersList.random().toString()
-        }
-
+    for (i in 1..3) {
+        resultPass = resultPass + CharList.random().toString() + NumbersList.random().toString()
     }
-//     resulrPass.forEach { print(it) } // вариант вывода массива строкой без запятых
-    print(resulrPass.joinToString(""))
+    print(resultPass)
 }
