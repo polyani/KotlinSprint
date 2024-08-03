9d69732a7506fad107a990fa67bce4d00aa9f257
polyani <167131294+polyani@users.noreply.github.com>
2024-08-03 20:43:24 +0300

    Delete src/main/kotlin/lesson_7/lesson7_task3.kt

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++
@@ -1,9 +0,0 @@
-package lesson_7
-
-fun main() {
-    val inputNumber: Int
-    print("Введите число - ")
-    inputNumber = readln().toInt()
-    val resRange = 0..inputNumber
-    println((resRange).filter { it % 2 == 0 })
-}
