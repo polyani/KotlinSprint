98f4eb5498947226ff52e8d4e710c2dfa78d5257
Nikolay.Polyakov <Nikolay.Polyakov@nexign.com>
2024-08-04 13:02:01 +0300

    KS-7-3 Commit corrected

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -5,6 +5,10 @@ fun main() {
     print("Введите число - ")
     inputNumber = readln().toInt()
     val resRange = 0..inputNumber
-    println((resRange).filter { it % 2 == 0 })
+    // println((resRange).filter { it % 2 == 0 }) // рабочий вариант
+
+    for (i in 0..inputNumber step 2) {
+        println(i)
+    }
 
 }
