12067c81149f14a413aed60960d2a821c8d1cdc7
polyani <ybrgjk826@gmail.com>
2024-08-25 18:48:31 +0300

    KS-9-3 переписан код - расчет кол-ва ингред-ов

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -2,18 +2,16 @@ package lesson_9
 
 fun main() {
     val printPortionRequest = "Введите количество порций - "
-    val ingredientOfRecipe = listOf("Яйцо", "Молоко", "Сливочное масло")
-    val countOfIngedient = mutableListOf(2, 50, 15)
+    val ingredientOfRecipe = mutableMapOf("Яйцо" to 2, "Молоко" to 50, "Сливочное масло" to 15)
     val unitOfMeasure = listOf("шт.", "мл.", "гр.")
-
+    var indexUnitOfMeasure = 0
     // запрос на количество порций
     print(printPortionRequest)
     val portion = readln().toInt()
 
-    for ((index, ingredient) in ingredientOfRecipe.withIndex()) {
-        print("${index + 1}. $ingredient ") // вывод номера в списке
-        print(countOfIngedient[index] * portion) // расчет и вывод рассчитываемого кол-ва
-        print(" ")  // пробел на выводе перед элементами из 3-го списка
-        println(unitOfMeasure[index])
+    for ((key, value) in ingredientOfRecipe) {
+        print("$key - ${value * portion} ")
+        println(unitOfMeasure[indexUnitOfMeasure])
+        indexUnitOfMeasure++
     }
 }
