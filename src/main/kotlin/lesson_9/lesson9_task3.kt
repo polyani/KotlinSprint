5e6b3f814dff96cf1b67206d16c76ba327ed64f3
polyani <ybrgjk826@gmail.com>
2024-09-01 11:08:40 +0300

    KS-9-3 исправлен код использована "map"

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -2,16 +2,11 @@ package lesson_9
 
 fun main() {
     val printPortionRequest = "Введите количество порций - "
-    val ingredientOfRecipe = mutableMapOf("Яйцо" to 2, "Молоко" to 50, "Сливочное масло" to 15)
-    val unitOfMeasure = listOf("шт.", "мл.", "гр.")
-    var indexUnitOfMeasure = 0
-    // запрос на количество порций
     print(printPortionRequest)
-    val portion = readln().toInt()
-
-    for ((key, value) in ingredientOfRecipe) {
-        print("$key - ${value * portion} ")
-        println(unitOfMeasure[indexUnitOfMeasure])
-        indexUnitOfMeasure++
+    val count = readln().toInt()
+    val ingredientOfRecipe = listOf("Яйцо - " to 2, "Молоко - " to 50, "Сливочное масло - " to 15)
+    val countForRecipe = ingredientOfRecipe.map { it.first + it.second * count }
+    countForRecipe.forEach {
+        println(it)
     }
 }
