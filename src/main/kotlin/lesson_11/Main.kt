a531d0c2aff984a3f0c05fd7ce602456325ea5d4
polyani <ybrgjk826@gmail.com>
2024-11-15 00:08:46 +0300

    KS-11-1 create class user

---
+++ src/main/kotlin/lesson_11/Main.kt
@@ -0,0 +1,25 @@
+package lesson_11
+
+fun main() {
+    val user1 = User(
+        userId = 1,
+        login = "Ivan",
+        password = (1..15000).random().toString(),
+        email = "testsample1@sample.com"
+    )
+    val user2 = User(
+        userId = 2,
+        login = "Petr",
+        password = (1..15000).random().toString(),
+        email = "testsample2@sample.com"
+    )
+    println(user1.userId)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
+
+    println(user2.userId)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
+}
---
+++ src/main/kotlin/lesson_11/User.kt
@@ -0,0 +1,6 @@
+package lesson_11
+
+class User(
+    val userId: Long, val login: String, val password: String, val email: String,
+    )  {
+}
